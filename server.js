e4e8f6c8a7063fa67c297f51ce7f1e48f863f119
Mehdi Druon <mehdidruon@protonmail.com>
2018-05-21 18:20:46 +0200

    Enable CORS on server

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ client.ping({
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', (req, res) => {
     res.sendFile('template.html',  { 
